23b8620d733a80d45277b357205ed751afab18b9
Huascar Cristian Cuellar Flores <huascarcristiancuellarflores@gmail.com>
2024-05-29 10:03:22 -0400

    Agregar User

--- login/src/Componentes/LoginForm/LoginForm.jsx
+++ login/src/Componentes/LoginForm/LoginForm.jsx
@@ -1,10 +1,30 @@
-//../Assets/fondo.mp4
-import React from "react";
+// window.location.href = "../../../../client/index.html"
+
+import React, { useState } from "react";
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import videobg from '../Assets/fondo.mp4';
 
 const LoginForm = () => {
+  // Definir los valores de username y password
+  const predefinedUsername = "cris";
+  const predefinedPassword = "cris123";
+
+  // Usar el estado para almacenar los valores de entrada del usuario
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    // Verificar si el nombre de usuario y la contraseña son correctos
+    if (username === predefinedUsername && password === predefinedPassword) {
+      // Redirigir a la página index.html en la carpeta client
+      window.location.href = "/client/index.html";
+    } else {
+      alert("Username or password is incorrect");
+    }
+  };
+
   return (
     <div className="background-video-wrapper">
       <video className="background-video" src={videobg} autoPlay loop muted />
@@ -13,11 +33,23 @@ const LoginForm = () => {
         <form action="">
           <h1>Login</h1>
           <div className="input-box">
-            <input type="text" placeholder="Username" required />
+            <input 
+              type="text" 
+              placeholder="Username" 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required 
+            />
             <FaUser className="icon" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required />
+            <input 
+              type="password" 
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required 
+            />
             <FaLock className="icon" />
           </div>
           <div className="remember-forgot">
@@ -27,7 +59,7 @@ const LoginForm = () => {
             </label>
             <a href="#">forgot password?</a>
           </div>
-          <button type="submit">Login</button>
+          <button type="submit" onClick={handleLoginClick}>Login</button>
           <div className="register-link">
             <p>
               No tienes cuenta? <a href="#">Register</a>
@@ -40,5 +72,3 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
-
-
